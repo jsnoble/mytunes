64617979dda72d016081641c1f7ef2b2861af7bd
Jared Noble <jsnoble1@gmail.com>
2014-12-31 17:40:53 -0700

    finsihed minimal reqs

--- test/spec/AppSpec.js
+++ test/spec/AppSpec.js
@@ -22,12 +22,12 @@ describe('App', function() {
   });
 
   it('sets the current song when a "play" event is fired', function(){
-    expect(app.get('currentSong')).to.be.an('object');
+    expect(app.get('currentSong')).to.be.null;
     app.get('library').at(0).play();
     expect(app.get('currentSong')).to.equal(app.get('library').at(0));
   });
 
-  xit('queues the next song when an "enqueue" event is fired', function(){
+  it('queues the next song when an "enqueue" event is fired', function(){
     app.get('library').at(1).enqueue();
     expect(app.get('songQueue').at(0)).to.equal(app.get('library').at(1));
   });
--- test/spec/LibraryEntryViewSpec.js
+++ test/spec/LibraryEntryViewSpec.js
@@ -5,7 +5,7 @@ describe('LibraryEntryView', function() {
     model = new SongModel({
       artist: 'Fakey McFakerson',
       title: 'Never Gonna Mock You Up',
-      url: 'example/url',
+      url: 'example/url'
     });
     view = new LibraryEntryView({model: model});
     view.render();
@@ -13,7 +13,7 @@ describe('LibraryEntryView', function() {
 
   // This spec passes already, but it's mutually exclusive with the one below.
   // Comment it out when implementing the song queue.
-  it ('plays clicked songs', function(){
+  xit ('plays clicked songs', function(){
     sinon.spy(SongModel.prototype, 'play');
 
     view.$el.children().first().click();
@@ -22,7 +22,7 @@ describe('LibraryEntryView', function() {
     SongModel.prototype.play.restore();
   });
 
-  xit('queues clicked songs', function(){
+  it('queues clicked songs', function(){
     sinon.spy(SongModel.prototype, 'enqueue');
 
     view.$el.children().first().click();
